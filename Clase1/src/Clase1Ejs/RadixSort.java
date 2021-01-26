7532ae60a4b6912582d4df2044bc48434f2116f0
Felipe Alfonso <felipearturo.alfonsoavilan@mercadolibre.com.co>
2021-01-26 08:57:17 -0500

    ejercicio 6, algoritmo radix sort

---
+++ Clase1/src/Clase1Ejs/RadixSort.java
@@ -0,0 +1,16 @@
+package Clase1Ejs;
+
+public class RadixSort {
+    public static void radixSort(int []arr){
+
+    }
+
+    public static void main(String[] args) {
+        int arr[]={16223,898,13,906,235,23,9,1532,6338,2511,8};
+        radixSort(arr);
+
+        for (int i:arr){
+            System.out.println(i);
+        }
+    }
+}
